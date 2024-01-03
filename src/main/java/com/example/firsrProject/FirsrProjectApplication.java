4d521c19bb82e9be85295f858bf31b93786b2832
Pasindu Perakum <it21160066my.sliit.lk>
2024-01-03 09:08:25 +0530

    Initial commit

---
+++ src/main/java/com/example/firsrProject/FirsrProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.firsrProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirsrProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirsrProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/firsrProject/FirsrProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.firsrProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirsrProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
